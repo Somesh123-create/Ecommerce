9bbcc077cf5184b3814e60d1ffd8b1cc4ab5f315
somesh hiremath <someshhiremath9@yahoo.com>
2021-06-06 21:01:06 +0530

    bootstrap

--- store/models.py
+++ store/models.py
@@ -7,6 +7,7 @@ class Product(models.Model):
     product_name = models.CharField(max_length=125)
     description = models.TextField()
     price = models.DecimalField(max_digits=19, decimal_places=2)
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
 
     def __str__(self):
         return self.product_name
--- store/views.py
+++ store/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Product
+from django.views.generic.detail import DetailView
 
 # Create your views here.
 
 
 class ProductListView(ListView):
     model = Product
-    template_name = 'store_list.html'
     context_object_name = 'product'
+    template_name = 'store_list.html'
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'store_detail.html'
+
+
 
 
